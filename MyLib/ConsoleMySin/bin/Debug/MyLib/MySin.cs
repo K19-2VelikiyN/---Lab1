ac77498782ab68a268ba97d7dc11200d4f6bf133
K19-2VelikiyN <79918656+K19-2VelikiyN@users.noreply.github.com>
2021-03-02 21:49:12 +0200

    Add files via upload

---
+++ MyLib/ConsoleMySin/bin/Debug/MyLib/MySin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    ///Обчислення математичних функцій
+
+    public class MySin
+    {
+       public static int Sin(int A, int B, int C)
+        {
+            int result;
+            int S1, S2;
+            S1 = A * B;
+            S2 = S1 / 6;
+            result = S2 / C;
+            return result;
+        }
+        public static double Sin1(int n, double x)
+        {
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s = s + (Math.Cos(2 * i * x) / i);
+            }
+            
+            return s;
+        }
+    }
+}
